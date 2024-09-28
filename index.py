4472cd506d9906583f78d4935326511abc2c17d0
quantsimp <kunriusho@gmail.com>
2024-09-28 01:46:27 -0700

    Add API endpoint to return the sum of two numbers

--- index.py
+++ index.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 
+@app.route('/sum', methods=['POST'])
+def sum_numbers():
+    data = request.get_json()
+    num1 = data.get('num1')
+    num2 = data.get('num2')
+    result = add_numbers(num1, num2)
+    return jsonify({'sum': result})
 # Run the app at the specified port
 if __name__ == '__main__':
     app.run(port=8010, debug=True)
